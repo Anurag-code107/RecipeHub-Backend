7ae19572caa5dff540c8b273e733f44a2b324a83
Anurag <anuragnegi2704@gmail.com>
2025-01-02 13:29:04 +0530

    added healthcheck in homecontroller seperately to use uptimerobot

--- src/main/java/com/negi/controller/HomeController.java
+++ src/main/java/com/negi/controller/HomeController.java
@@ -5,9 +5,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
-
-    @GetMapping()
-    public String homeCotroller() {
+    @GetMapping("/api/health")
+    public String healthCheck() {
         return "Backend is running well";
+    }
+
+    @GetMapping("/")
+    public String homeController() {
+        return "Welcome to RecipeHub Backend!";
     }
 }
